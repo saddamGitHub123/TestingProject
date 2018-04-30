342da6fdaefe0d7979d47437f308f31e517011e6
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-04-30 18:04:09 +0530

    change home pge , root and login page using jsp and some fornt end done

--- apitest/src/main/java/com/backend/apitest/controller/PageController.java
+++ apitest/src/main/java/com/backend/apitest/controller/PageController.java
@@ -5,10 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.backend.restbackend.user.model.LoginResponse;
 
 
 /**
@@ -16,7 +15,7 @@ import com.backend.restbackend.user.model.LoginResponse;
  * @author saddam
  *
  */
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @Controller
 @EnableWebMvc
 
@@ -31,14 +30,69 @@ public class PageController {
 
 	//This is for home page 
 	
-	@RequestMapping(value = { "/", "/home", "/index" })
+	@RequestMapping(value = {"/home", "/index" })
 	public ModelAndView index() {
 		logger.info("This is the PageController ");
 
+	//	ModelAndView mv = new ModelAndView("page");
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "      Welcome to Spring And Hibernate World ");
+		//mv.addObject("greeting", "      Welcome to Spring And Hibernate World ");
 		return mv;
 	}
+	@RequestMapping(value = {"/userlogin" })
+	public ModelAndView LoginPage() {
+		logger.info("This is the PageController ");
+
+	//	ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv = new ModelAndView("welCome");
+		//mv.addObject("greeting", "      Welcome to Spring And Hibernate World ");
+		return mv;
+	}
+	@RequestMapping(value = {"/e_home" })
+	public ModelAndView E_HomePage() {
+		logger.info("This is the PageController ");
+
+	//	ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv = new ModelAndView("ShoppingHome");
+		//mv.addObject("greeting", "      Welcome to Spring And Hibernate World ");
+		return mv;
+	}
+	
+//	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+//	public ModelAndView welcomePage() {
+//
+//		ModelAndView model = new ModelAndView();
+//		model.addObject("title", "Spring Security Hello World");
+//		model.addObject("message", "This is welcome page!");
+//		model.setViewName("hello");
+//		return model;
+//
+//	}
+//
+//	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+//	public ModelAndView adminPage() {
+//
+//		ModelAndView model = new ModelAndView();
+//		model.addObject("title", "Spring Security Hello World");
+//		model.addObject("message", "This is protected page - Admin Page!");
+//		model.setViewName("admin");
+//
+//		return model;
+//
+//	}
+//
+//	@RequestMapping(value = "/dba**", method = RequestMethod.GET)
+//	public ModelAndView dbaPage() {
+//
+//		ModelAndView model = new ModelAndView();
+//		model.addObject("title", "Spring Security Hello World");
+//		model.addObject("message", "This is protected page - Database Page!");
+//		model.setViewName("admin");
+//
+//		return model;
+//
+//	}
+	
 	/*@RequestMapping(value = "/register", method = RequestMethod.GET)
 	  public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
 	    ModelAndView mav = new ModelAndView("register");
